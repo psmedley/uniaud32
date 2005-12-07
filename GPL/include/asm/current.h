eb00eff8aef0868d40d90ed630d5381c405a9fa8
vladest <vladest@5c76a2b7-d805-0410-b276-61f8b3678bae>
2005-12-07 19:32:06 +0000

    Deleted include

--- GPL/include/asm/current.h
+++
@@ -1,8 +0,0 @@
-/* $Id: current.h,v 1.1.1.1 2003/07/02 13:56:58 eleph Exp $ */
-
-#ifndef _I386_CURRENT_H
-#define _I386_CURRENT_H
-
-extern struct task_struct *current;
-
-#endif /* !(_I386_CURRENT_H) */
--- GPL/include/linux/kmod.h
+++
@@ -1,6 +0,0 @@
-/* $Id: kmod.h,v 1.1.1.1 2003/07/02 13:57:00 eleph Exp $ */
-
-#ifndef _LINUX_KMOD_H
-#define _LINUX_KMOD_H
-
-#endif /* _LINUX_KMOD_H */
