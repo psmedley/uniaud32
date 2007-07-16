5f207e471dd82310fd0aacbacf4d691f45e33cec
gentux2 <gentux2@5c76a2b7-d805-0410-b276-61f8b3678bae>
2007-07-16 01:24:57 +0000

    Tag after merge to Alsa 0.9.0rc6

---
+++ GPL/tags/Alsa-ReSync-0.9.0rc6/alsa-kernel/include/sound/version.h
@@ -0,0 +1,10 @@
+/*
+ *  Configuration header file for compilation of the ALSA driver
+ */
+#define CONFIG_SND_DATE "04-29-2007"
+
+#ifndef __ALSA_VERSION_H__
+#define __ALSA_VERSION_H__
+
+
+#endif //__ALSA_VERSION_H__
