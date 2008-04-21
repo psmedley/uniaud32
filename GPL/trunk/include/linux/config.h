a30fe207b80ad4d6e0b7d5a4a15a4ded03b519b1
gentux2 <gentux2@5c76a2b7-d805-0410-b276-61f8b3678bae>
2008-04-21 00:46:45 +0000

    Adjustments to linux headers and lib32 for compiling resync 1.0.4

--- GPL/trunk/include/linux/config.h
+++ GPL/trunk/include/linux/config.h
@@ -1,6 +1,10 @@
-/* $Id: config.h,v 1.1.1.1 2003/07/02 13:57:00 eleph Exp $ */
 
 #ifndef _LINUX_CONFIG_H
 #define _LINUX_CONFIG_H
 
+/* __user not supported in OpenWatcom */
+#ifndef __user
+#define __user
 #endif
+
+#endif /* _LINUX_CONFIG_H */
