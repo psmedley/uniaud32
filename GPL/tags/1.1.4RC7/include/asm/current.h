81ab3bff600eef79c26ccb14d056fff6d69c677c
gentux2 <gentux2@5c76a2b7-d805-0410-b276-61f8b3678bae>
2009-02-12 05:21:56 +0000

    Tag of Uniaud32 1.1.4RC7

---
+++ GPL/tags/1.1.4RC7/include/asm/current.h
@@ -0,0 +1,8 @@
+/* $Id: current.h,v 1.1.1.1 2003/07/02 13:56:58 eleph Exp $ */
+
+#ifndef _I386_CURRENT_H
+#define _I386_CURRENT_H
+
+extern struct task_struct *current;
+
+#endif /* !(_I386_CURRENT_H) */
---
+++ GPL/tags/1.1.4RC7/include/linux/dma-mapping.h
@@ -0,0 +1,26 @@
+#ifndef _ASM_LINUX_DMA_MAPPING_H
+#define _ASM_LINUX_DMA_MAPPING_H
+
+/*
+ * This file is part of Uniaud32.
+ * Copyright (c) 2008 Netlabs
+ *
+ *  Uniaud32 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  Uniaud32 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Uniaud32.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * dma-mapping.h --- Linux DMA mapping for OS/2 and eComStation.
+ */
+
+
+#endif /* _ASM_LINUX_DMA_MAPPING_H */
