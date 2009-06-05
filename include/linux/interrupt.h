350f4fcf2f14fa93c874940d4ac60e343f83bd12
Paul Smedley <paul@smedley.id.au>
2009-06-05 21:24:02 +0000

    Move definitions from config.h to interrupt.h

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -1,5 +1,3 @@
-/* $Id: interrupt.h,v 1.1.1.1 2003/07/02 13:57:00 eleph Exp $ */
-
 /* interrupt.h */
 #ifndef _LINUX_INTERRUPT_H
 #define _LINUX_INTERRUPT_H
@@ -7,6 +5,26 @@
 #include <linux/kernel.h>
 //#include <asm/bitops.h>
 #include <asm/atomic.h>
+
+/*
+ * For 2.4.x compatibility, 2.4.x can use
+ *
+ *	typedef void irqreturn_t;
+ *	#define IRQ_NONE
+ *	#define IRQ_HANDLED
+ *	#define IRQ_RETVAL(x)
+ *
+ * To mix old-style and new-style irq handler returns.
+ *
+ * IRQ_NONE means we didn't handle it.
+ * IRQ_HANDLED means that we did have a valid interrupt and handled it.
+ * IRQ_RETVAL(x) selects on the two depending on x being non-zero (for handled)
+ */
+typedef int irqreturn_t;
+
+#define IRQ_NONE	(0)
+#define IRQ_HANDLED	(1)
+#define IRQ_RETVAL(x)	((x) != 0)
 
 struct irqaction {
 	void (*handler)(int, void *, struct pt_regs *);
