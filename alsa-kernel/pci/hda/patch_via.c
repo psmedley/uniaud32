b2c777219963fc59a0ad07aa09fc66f47123162f
Paul Smedley <paul@smedley.id.au>
2021-01-15 08:33:10 +0000

    Bump linux source to kernel 5.10.7

--- alsa-kernel/pci/hda/patch_via.c
+++ alsa-kernel/pci/hda/patch_via.c
@@ -1010,6 +1010,7 @@ static const struct hda_verb vt1802_init_verbs[] = {
 enum {
 	VIA_FIXUP_INTMIC_BOOST,
 	VIA_FIXUP_ASUS_G75,
+	VIA_FIXUP_POWER_SAVE,
 };
 
 static void via_fixup_intmic_boost(struct hda_codec *codec,
@@ -1027,6 +1028,13 @@ static const struct hda_pintbl VIA_FIXUP_ASUS_G75_PINS[] = {
 			{0}
 		};
 #endif
+
+static void via_fixup_power_save(struct hda_codec *codec,
+				 const struct hda_fixup *fix, int action)
+{
+	if (action == HDA_FIXUP_ACT_PRE_PROBE)
+		codec->power_save_node = 0;
+}
 
 static const struct hda_fixup via_fixups[] = {
 	[VIA_FIXUP_INTMIC_BOOST] = {
@@ -1046,11 +1054,16 @@ static const struct hda_fixup via_fixups[] = {
 		.v.pins = VIA_FIXUP_ASUS_G75_PINS
 #endif
 	},
+	[VIA_FIXUP_POWER_SAVE] = {
+		.type = HDA_FIXUP_FUNC,
+		.v.func = via_fixup_power_save,
+	},
 };
 
 static const struct snd_pci_quirk vt2002p_fixups[] = {
 	SND_PCI_QUIRK(0x1043, 0x1487, "Asus G75", VIA_FIXUP_ASUS_G75),
 	SND_PCI_QUIRK(0x1043, 0x8532, "Asus X202E", VIA_FIXUP_INTMIC_BOOST),
+	SND_PCI_QUIRK(0x1558, 0x3501, "Clevo W35xSS_370SS", VIA_FIXUP_POWER_SAVE),
 	{0}
 };
 
