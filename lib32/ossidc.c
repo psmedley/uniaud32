95131e5c301468003981be561ace4acb6f82273d
Paul Smedley <paul@smedley.id.au>
2021-01-25 01:06:16 +0000

    Add alsa_seq_device_exit to shutdown routine, source tidyups

--- lib32/ossidc.c
+++ lib32/ossidc.c
@@ -65,33 +65,33 @@ typedef struct cardcalls_t {
 }cardcalls_t;
 
 cardcalls_t cardcalls[CARDS_NUM] = {
-	{ CARD_ICH,		 &name_module_init(intel8x0_driver_init),	&name_module_exit(intel8x0_driver_exit)	  },
-	{ CARD_VIA82XX,	 &name_module_init(via82xx_driver_init),	 	&name_module_exit(via82xx_driver_exit)	  },
-	{ CARD_SBLIVE,	 &name_module_init(emu10k1_driver_init),		&name_module_exit(emu10k1_driver_exit)	  },
+	{ CARD_ICH,	 &name_module_init(intel8x0_driver_init),	&name_module_exit(intel8x0_driver_exit)	  },
+	{ CARD_VIA82XX,	 &name_module_init(via82xx_driver_init),	&name_module_exit(via82xx_driver_exit)	  },
+	{ CARD_SBLIVE,	 &name_module_init(emu10k1_driver_init),	&name_module_exit(emu10k1_driver_exit)	  },
 	{ CARD_CMEDIA,	 &name_module_init(cmipci_driver_init),		&name_module_exit(cmipci_driver_exit)	  },
-	{ CARD_ALS4000,	 &name_module_init(als4000_driver_init),		&name_module_exit(als4000_driver_exit)	  },
+	{ CARD_ALS4000,	 &name_module_init(als4000_driver_init),	&name_module_exit(als4000_driver_exit)	  },
 	{ CARD_CS4281,	 &name_module_init(cs4281_driver_init),		&name_module_exit(cs4281_driver_exit)	  },
 	{ CARD_CS46XX,	 &name_module_init(cs46xx_driver_init),		&name_module_exit(cs46xx_driver_exit)	  },
 	{ CARD_CS5535,	 &name_module_init(cs5535audio_driver_init),	&name_module_exit(cs5535audio_driver_exit) },
 	{ CARD_ESS1938,	 &name_module_init(es1938_driver_init),		&name_module_exit(es1938_driver_exit)	  },
-	{ CARD_ENSONIQ0, &name_module_init(ens137x_driver_init),		&name_module_exit(ens137x_driver_exit)	  },
-	{ CARD_ENSONIQ1, NULL,		NULL },
+	{ CARD_ENSONIQ0, &name_module_init(ens137x_driver_init),	&name_module_exit(ens137x_driver_exit)	  },
+	{ CARD_ENSONIQ1, NULL,						NULL 					  },
 	{ CARD_YAMAHA,	 &name_module_init(ymfpci_driver_init),		&name_module_exit(ymfpci_driver_exit)	  },
 	{ CARD_MAESTRO,	 &name_module_init(es1968_driver_init),		&name_module_exit(es1968_driver_exit)	  },
-	{ CARD_MAESTRO3, &name_module_init(m3_driver_init),			&name_module_exit(m3_driver_exit)		  },
-	{ CARD_ALI5451,	 &name_module_init(ali5451_driver_init),			&name_module_exit(ali5451_driver_exit)		  },
-	{ CARD_TRIDENT,	 &name_module_init(trident_driver_init),		&name_module_exit(trident_driver_exit)	  },
+	{ CARD_MAESTRO3, &name_module_init(m3_driver_init),		&name_module_exit(m3_driver_exit)	  },
+	{ CARD_ALI5451,	 &name_module_init(ali5451_driver_init),	&name_module_exit(ali5451_driver_exit)	  },
+	{ CARD_TRIDENT,	 &name_module_init(trident_driver_init),	&name_module_exit(trident_driver_exit)	  },
 #ifdef VORTEX
 	{ CARD_VORTEX,	 &name_module_init(vortex_driver_init),		&name_module_exit(vortex_driver_exit)	  },
 #else
-	{ CARD_VORTEX,	 NULL,		NULL },
+	{ CARD_VORTEX,	 NULL,						NULL					  },
 #endif
-	{ CARD_NEOMAGIC, &name_module_init(nm256_driver_init),		&name_module_exit(nm256_driver_exit)		  },
-	{ CARD_FM801,	 &name_module_init(fm801_driver_init),		&name_module_exit(fm801_driver_exit)		  },
+	{ CARD_NEOMAGIC, &name_module_init(nm256_driver_init),		&name_module_exit(nm256_driver_exit)	  },
+	{ CARD_FM801,	 &name_module_init(fm801_driver_init),		&name_module_exit(fm801_driver_exit)	  },
 	{ CARD_ATIIXP,	 &name_module_init(atiixp_driver_init),		&name_module_exit(atiixp_driver_exit)	  },
 	{ CARD_AUDIGYLS, &name_module_init(ca0106_driver_init),		&name_module_exit(ca0106_driver_exit)	  },
-	{ CARD_AZX,		 &name_module_init(azx_driver_init),			&name_module_exit(azx_driver_exit)		  },
-	{ CARD_BT87X,	 &name_module_init(alsa_card_bt87x_init),		&name_module_exit(alsa_card_bt87x_exit)		  },
+	{ CARD_AZX,	 &name_module_init(azx_driver_init),		&name_module_exit(azx_driver_exit)	  },
+	{ CARD_BT87X,	 &name_module_init(alsa_card_bt87x_init),	&name_module_exit(alsa_card_bt87x_exit)	  },
 };
 
 int 	   nrCardsDetected = 0;
@@ -114,21 +114,18 @@ OSSRET OSS32_Initialize(void)
 	}
 	dprintf(("OSS32_Initialize: Start address: %X", OffsetBeginCS32));
 
-	if(call_module_init(alsa_sound_init) != 0)		 return OSSERR_INIT_FAILED;
-	if(call_module_init(alsa_seq_device_init) != 0)		 return OSSERR_INIT_FAILED;
+	if(call_module_init(alsa_sound_init) != 0)		return OSSERR_INIT_FAILED;
+	if(call_module_init(alsa_seq_device_init) != 0)		return OSSERR_INIT_FAILED;
 	dprintf(("OSS32_Initialize1: Start address: %X", OffsetBeginCS32));
-	if(call_module_init(alsa_pcm_init) != 0)		 return OSSERR_INIT_FAILED;
-	if(call_module_init(alsa_hwdep_init) != 0)		 return OSSERR_INIT_FAILED;
-	if(call_module_init(alsa_timer_init) != 0)		 return OSSERR_INIT_FAILED;
+	if(call_module_init(alsa_pcm_init) != 0)		return OSSERR_INIT_FAILED;
+	if(call_module_init(alsa_hwdep_init) != 0)		return OSSERR_INIT_FAILED;
+	if(call_module_init(alsa_timer_init) != 0)		return OSSERR_INIT_FAILED;
 
-	if(call_module_init(alsa_rawmidi_init) != 0)	 return OSSERR_INIT_FAILED;
-	if(call_module_init(alsa_seq_init) != 0)		 return OSSERR_INIT_FAILED;
-//	if(call_module_init(alsa_opl3_init) != 0)		 return OSSERR_INIT_FAILED;
-//	if(call_module_init(alsa_mpu401_uart_init) != 0) return OSSERR_INIT_FAILED;
+	if(call_module_init(alsa_rawmidi_init) != 0)	 	return OSSERR_INIT_FAILED;
+	if(call_module_init(alsa_seq_init) != 0)		return OSSERR_INIT_FAILED;
 	call_module_init(opl3_seq_driver_init);	 
-//	call_module_init(ac97_bus_init);
 	call_module_init(regmap_initcall);
-#if 1
+
 	/* Init functions for HDA audio */
 	call_module_init(hda_bus_init);
 	call_module_init(generic_driver_init);
@@ -141,7 +138,7 @@ OSSRET OSS32_Initialize(void)
 	call_module_init(realtek_driver_init);
 	call_module_init(sigmatel_driver_init);
 	call_module_init(via_driver_init);
-#endif
+
 	//dprintf(("OSS32_Initialize: ForceCard=%d", ForceCard));
 
 	for (sI=0; sI<CARDS_NUM; sI++) {
@@ -183,14 +180,13 @@ OSSRET OSS32_Shutdown(void)
 		if(fnCardExitCall[i]) fnCardExitCall[i]();
 	}
 
-//	call_module_exit(alsa_mpu401_uart_exit);
 	call_module_exit(opl3_seq_driver_exit);
-//	call_module_exit(alsa_opl3_exit);
 	call_module_exit(alsa_seq_exit);
 	call_module_exit(alsa_rawmidi_exit);
 	call_module_exit(alsa_timer_exit);
 	call_module_exit(alsa_hwdep_exit);
 	call_module_exit(alsa_pcm_exit);
+	call_module_exit(alsa_seq_device_exit);
 	call_module_exit(alsa_sound_exit);
 
 	return OSSERR_SUCCESS;
